adee2051f3228cf59486012e97a5d29b1886b156
Mohamed-L <mohamedlarson11@gmail.com>
2026-03-11 15:19:01 -0400

    Initial commit

---
+++ src/main.ino
@@ -0,0 +1,238 @@
+#include <PinChangeInterrupt.h>
+#include "message_comp.h"
+#include "ui.h"
+#include "transmit.h"
+
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+//CE, CSN for rf
+RF24 radio(9, 10);
+
+//potentially changeable constants
+const int sck = 13;
+const int miso = 12;
+const int mosi = 11;
+
+const int encoder_B = 8;
+const int encoder_A = 7;
+
+const int selectBtn = 6;
+const int leftArrow = 5;
+const int upArrow = 4;
+const int rightArrow = 3;
+const int downArrow = 2;
+
+//globals and or variables used in the loop
+enum Mode {RX, TX }; 
+int current_mode = TX;
+bool rxListen = true;
+int rxMsgIndex = 0;
+int rxCursorPos = 0;
+
+//time since last keypapd press
+unsigned long lastLeftPress = 0;
+unsigned long lastRightPress = 0;
+unsigned long lastUp = 0;
+unsigned long lastDown = 0;
+
+//Pinmode interrupt so that the encoder does skip and acts better
+void encoder_interrupt() {
+  readEncoder(encoder_A, encoder_B);
+}
+
+void setup() {
+  radio.begin();
+  lcd.init();
+  lcd.backlight();
+  init_radio_tx(1, 1);
+
+  pinMode(encoder_A, INPUT_PULLUP);
+  pinMode(encoder_B, INPUT_PULLUP);
+  pinMode(selectBtn, INPUT_PULLUP);
+  pinMode(upArrow, INPUT_PULLUP);
+  pinMode(downArrow, INPUT_PULLUP);
+  pinMode(leftArrow, INPUT_PULLUP);
+  pinMode(rightArrow, INPUT_PULLUP);
+  attachPCINT(digitalPinToPCINT(encoder_A), encoder_interrupt, CHANGE);
+}
+
+void loop() {
+  //wow system interrupts are so cool, i never need to poll. but -30bytes ram :(
+  if (current_mode == RX) {
+
+    if (rxListen) {
+      if (radio.available()) {
+        if (radio.testRPD()) {
+          int status = receive_packet();
+          rx_count(status, lcd);
+          if (status == 1) {
+            char* message = rebuild_message();
+            if (message != NULL) {
+              insert_msg_head(message);
+              rxMsgIndex = 0;
+            } else {
+              lcd.setCursor(0, 0);
+              lcd.print(F("Rebuild Failed  "));
+              delay(1000);
+            }
+          }
+        } else {
+          radio.flush_rx();
+        }
+      }
+    }
+
+    int selectAction = handleSelect(selectBtn, lcd);
+
+    if (selectAction == LONG_SELECT) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print(F("   TX Mode      "));
+      currentSet = 0;
+      encoderPos = constrain(encoderPos, 0, charSets[currentSet].size - 1);
+      insertPos = 0;
+      updateCursor = 0;
+      editMode = false;
+      current_mode = TX;
+      delete_all_data();
+      init_radio_tx(2, 1);
+      delay(500);
+      return;
+    }
+
+    int msgCount = saved_msgs();
+
+    //Short select deletes current message when browsing
+    if (selectAction == SHORT_SELECT && !rxListen && msgCount > 0) {
+      MsgNode *currentMsg = msg_head;
+      for (int i = 0; i < rxMsgIndex && currentMsg != NULL; i++)
+        currentMsg = currentMsg->next;
+
+      if (currentMsg != NULL) {
+        delete_msg(currentMsg);
+        msgCount = saved_msgs();
+        
+        if (msgCount == 0) {
+          rxListen = true;
+          rxMsgIndex = 0;
+          ui_listen(lcd);
+        } else if(rxMsgIndex >= msgCount) {
+          rxMsgIndex = msgCount - 1;
+        }
+      }
+    }
+
+    //Right button switch to browse mode
+    if (buttonPressed(rightArrow, lastRightPress)) {
+      rxListen = false;
+      rxCursorPos = 0;
+    }
+    //Left button switch to listen mode
+    if (buttonPressed(leftArrow, lastLeftPress)) {
+      rxListen = true;
+      ui_listen(lcd);
+    }
+
+    // Up/Down cycle through messages when browsing
+    if (!rxListen && msgCount > 0) {
+      if (buttonPressed(upArrow, lastUp)) {
+        if (rxMsgIndex > 0) {
+          rxMsgIndex--;
+          rxCursorPos = 0;
+        }
+      }
+      if (buttonPressed(downArrow, lastDown)) {
+        if (rxMsgIndex < msgCount - 1) {
+          rxMsgIndex++;
+          rxCursorPos = 0;
+        }
+      }
+
+      MsgNode *currentMsg = msg_head;
+      for (int i = 0; i < rxMsgIndex && currentMsg != NULL; i++)
+        currentMsg = currentMsg->next;
+
+      if (currentMsg != NULL) {
+        rx_display_message(currentMsg->data, rxCursorPos, rxMsgIndex, lcd);
+      }
+    }
+  }
+
+  else {
+    int selectAction = handleSelect(selectBtn, lcd);
+
+    //Long press sends message and switch to RX
+    if (selectAction == LONG_SELECT) {
+      transmit_message(lcd);
+
+      int messageLen = saved_msgs();
+
+      if (messageLen > 0) {
+        delete_all_data();
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print(F("   MSG Sent     "));
+      }
+      else {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print(F("   RX Mode      "));
+        init_radio_rx(1,1);
+      }
+      insertPos = 0;
+      updateCursor = 0;
+      editMode = false;
+      current_mode = RX;
+      rxListen = true;
+
+      delay(500);
+      return;
+    }
+
+    //L/R cycle character layer in compose mode
+    if (!editMode) {
+      if (buttonPressed(rightArrow, lastRightPress)) {
+        currentSet = (currentSet + 1) % 3;
+        encoderPos = 0;  //reset to center
+      }
+      if (buttonPressed(leftArrow, lastLeftPress)) {
+        currentSet = (currentSet - 1 + 3) % 3;
+        encoderPos = 0;
+      }
+    }
+
+    //Down arrow enters edit mode
+    if (buttonPressed(downArrow, lastDown)) {
+      if (!editMode && get_message_length() > 0) {
+        editMode = true;
+        updateCursor = constrain(insertPos - 1, 0, get_message_length() - 1);
+        encoderPos = updateCursor;  //set encoder to cursor pos
+      }
+    }
+
+    //Up arrow exits edit mode
+    if (buttonPressed(upArrow, lastUp)) {
+      if (editMode) {
+        editMode = false;
+        insertPos = constrain(updateCursor + 1, 0, get_message_length());
+        encoderPos = 0;  //reset for compose mode
+      } 
+    }
+
+    //Displays LCD
+    if (editMode) {
+      if (get_message_length() > 0) {
+        encoderPos = constrain(encoderPos, 0, get_message_length() - 1);
+        updateCursor = encoderPos;
+      }
+      tx_display_message(updateCursor, true, lcd);
+    }
+    else {
+      encoderPos = constrain(encoderPos, 0, charSets[currentSet].size - 1);
+      letter_scroll(encoderPos, lcd);
+      int messageLength = get_message_length();
+      insertPos = constrain(insertPos, 0, messageLength);
+      tx_display_message(insertPos, false, lcd);
+    }
+  }
+}
---
+++ src/ui.cpp
@@ -0,0 +1,217 @@
+#include "ui.h"
+#include "message_comp.h"
+#include <LiquidCrystal_I2C.h>
+
+//Debounce times
+const int DEBOUNCE_MS   = 100;
+const int LONG_PRESS_MS = 600;
+
+unsigned long lastSelect = 0;
+bool selectHeld = false;
+bool longPressDone = false;
+
+volatile int encoderPos = 0;
+volatile int lastTick = HIGH;
+
+bool editMode = false;
+int  insertPos = 0;
+int  updateCursor = 0;
+
+//Hardware interfacing functions
+bool buttonPressed(int pin, unsigned long &lastPress) {
+  if (digitalRead(pin) || millis() - lastPress <= DEBOUNCE_MS) return false;
+  lastPress = millis();
+  return true;
+}
+
+void readEncoder(int pinA, int pinB) {
+  int aState = digitalRead(pinA);
+  if(aState != lastTick && aState == LOW){
+    digitalRead(pinB) == HIGH ? encoderPos++ : encoderPos--;
+  }
+  lastTick = aState;
+}
+
+int handleSelect(int pin, LiquidCrystal_I2C &lcd) {
+  bool pressed = (digitalRead(pin) == LOW);
+  int state = SELECT_NONE;
+
+  //Press starts recording the time
+  if (pressed && !selectHeld) {
+    selectHeld = true;
+    longPressDone = false;
+    lastSelect = millis();
+  }
+
+  //Held, check if long press threshold reached
+  else if (pressed && !longPressDone) {
+    if (millis() - lastSelect > LONG_PRESS_MS) {
+      longPressDone = true;
+      state = LONG_SELECT;
+    }
+  }
+
+  //Released, if it wasn't a long press, its a short press
+  else if (!pressed && selectHeld) {
+    if (!longPressDone && (millis() - lastSelect > DEBOUNCE_MS)) {
+      if (!editMode) {
+        int setSize = charSets[currentSet].size;
+        if (setSize > 0) {
+          char c = pgm_read_byte(&charSets[currentSet].chars[constrain(encoderPos, 0, setSize - 1)]);
+          Node *target = (insertPos > 0) ? get_node_at(insertPos - 1) : nullptr;
+          int result = target ? insert_data_at_middle(target, c) : insert_data_at_head(c);
+          if (result == 0) insertPos++;
+        }
+      } else {
+        int len = get_message_length();
+        if (len > 0) {
+          find_and_delete_data(get_node_at(updateCursor));
+          len--;
+          if (len == 0) { updateCursor = 0; insertPos = 0; }
+          else updateCursor = min(updateCursor, len - 1);
+        }
+      }
+      state = SHORT_SELECT;
+    }
+    selectHeld = false;
+    longPressDone = false;
+  }
+
+  return state;
+}
+
+void letter_scroll(int letter_index, LiquidCrystal_I2C &lcd) {
+  //clear row
+  lcd.setCursor(0, 0);
+  lcd.print(F("                "));
+
+  int setSize = charSets[currentSet].size;
+  if (setSize == 0) return;
+  
+  // Constrain letter_index to valid range
+  letter_index = constrain(letter_index, 0, setSize - 1);
+
+  //main letter choices
+  if (letter_index > 0) {
+    lcd.setCursor(4, 0);
+    lcd.print("< ");
+    lcd.print((char)pgm_read_byte(&charSets[currentSet].chars[letter_index - 1]));
+  }
+  lcd.setCursor(8, 0);
+  lcd.print((char)pgm_read_byte(&charSets[currentSet].chars[letter_index]));
+  if (letter_index < setSize - 1) {
+    lcd.setCursor(10, 0);
+    lcd.print((char)pgm_read_byte(&charSets[currentSet].chars[letter_index + 1]));
+    lcd.print(" >");
+  }
+}
+
+//2 DISPLAY FUNCTIONS... ONE FOR THE MESSAGE COMPOSE WRITING ONE FOR SCROLLING AND EDITING THE MESSAGE
+//possible to combine ltr
+void tx_display_message(int cursorPos, bool editMode, LiquidCrystal_I2C &lcd) {
+  int messageLength = get_message_length();
+
+  if (editMode) {
+    lcd.setCursor(0, 0);
+    lcd.print(F("Edit Msg:Sel=Del"));
+  }
+
+  lcd.setCursor(0, 1);
+  lcd.print(F("                "));
+
+  if (messageLength == 0) {
+    if (!editMode && (millis() / 500) % 2 == 0) {
+      lcd.setCursor(0, 1);
+      lcd.print("_");
+    }
+    return;
+  }
+  cursorPos = constrain(cursorPos, 0, messageLength);
+
+  int maxVisible = editMode ? 14 : 15;
+  int windowStart = max(0, cursorPos - maxVisible);
+
+  Node *p = get_node_at(windowStart);
+  lcd.setCursor(0, 1);
+  for (int i = windowStart, col = 0; i < messageLength && col < 16; i++, col++, p = p->next) {
+    char c = p->data;
+    if (editMode && i == cursorPos)
+      lcd.write((millis() / 300) % 2 == 0 ? (uint8_t)c : 0xFF);
+    else
+      lcd.print(c);
+  }
+
+  if (!editMode) {
+    int cursorCol = cursorPos - windowStart;
+    if (cursorCol < 16 && (millis() / 500) % 2 == 0) {
+      lcd.setCursor(cursorCol, 1);
+      lcd.print("_");
+    }
+  }
+}
+
+void tx_show_translation(char *msg, LiquidCrystal_I2C &lcd){
+
+}
+
+
+void rx_display_message(char *msg, int cursorPos, int msgIndex, LiquidCrystal_I2C &lcd){
+  lcd.setCursor(0, 0);
+  lcd.print("                ");
+  lcd.setCursor(0, 0);
+  lcd.print("Msg ");
+  lcd.print(msgIndex + 1);
+
+  int len = strlen(msg);
+
+  lcd.setCursor(0, 1);
+  lcd.print("                ");
+  lcd.setCursor(0, 1);
+  for (int i = cursorPos, col = 0; i < len && col < 16; i++, col++) {
+    lcd.print(msg[i]);
+  }
+}
+
+void progress_bar(int packet_index, int totalPackets, LiquidCrystal_I2C &lcd){
+  lcd.setCursor(0, 0);
+  lcd.print(F("Sending "));
+  lcd.print(packet_index + 1);
+  lcd.print("/");
+  lcd.print(totalPackets);
+
+  int percent = ((packet_index + 1) * 100)/totalPackets;
+  int barLength = map(percent, 0, 100, 0, 13);
+
+  lcd.setCursor(0, 1);
+  for (int i = 0; i < 13; i++) {
+    if(i < barLength) lcd.write(0xFF);
+    else lcd.print(' ');
+  }
+  lcd.setCursor(13, 1);
+  if (percent < 10) lcd.print("  ");
+  else if (percent < 100) lcd.print(" ");
+  lcd.print(percent);
+}
+
+void rx_count(int packet_index, LiquidCrystal_I2C &lcd){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(F(" Receiving Msg! "));
+  lcd.setCursor(0, 1);
+  lcd.print(" Pkt: ");
+  lcd.print(packet_index + 1);
+  lcd.print("/");
+  lcd.print("?");
+}
+
+void memory_full(LiquidCrystal_I2C &lcd){
+  lcd.setCursor(0, 0);
+  lcd.print(F("  No Memory :(  "));
+}
+
+void ui_listen(LiquidCrystal_I2C &lcd){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(F("  Listening...  "));
+}
+
